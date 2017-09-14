6444a2a3c19bb425c43278b4dda08acd97d9f367
Basu macbookpro <basu@shrigowri.com>
2017-09-14 15:35:42 +0530

    first commit

---
+++ testing_travis/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testing_travis
+//
+//  Created by Basavaraj Kalaghatagi on 14/09/17.
+//  Copyright © 2017 SGS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
